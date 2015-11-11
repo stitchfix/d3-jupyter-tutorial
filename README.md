0f884be0320a872c8f1b032e6e1ce72f32eacbf7
bricofStitchFix <bcoffey@stitchfix.com>
2015-11-10 23:21:42 -0500

    minor cleaning

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # d3-jupyter-tutorial
+
+This repo contains Jupyter notebooks, along with related code and example data, for a tutorial on the use of D3 in Jupyter. 
+
+The presentation file `PyData-D3-in-Jupyter.pdf` provides an entry point and overview.
---
+++ d3_lib.py
@@ -0,0 +1,38 @@
+import random
+import inspect, os
+from string import Template
+
+
+def this_dir():
+    this_file = inspect.getfile(inspect.currentframe())
+    return os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+
+
+def set_styles(css_file_names):
+    if type(css_file_names) == str:
+        style = open(this_dir() + '/css/' + css_file_names + '.css','r').read()
+    else:
+        style = ''
+        for css_file_name in css_file_names:
+            style += open(this_dir() + '/css/' + css_file_name + '.css','r').read()
+    return "<style>" + style + "</style>"
+
+
+def draw_graph(type, data):
+
+    JS_text = Template('''
+    
+                <div id='maindiv${divnum}'></div>
+                
+                <script>
+                    $main_text
+                </script>
+
+                ''')
+
+    divnum = int(random.uniform(0,9999999999))
+    
+    main_text_template = Template( open(this_dir() + '/js/' + type + '.js','r').read() )
+    
+    main_text = main_text_template.safe_substitute({'divnum': divnum, 'data': str(data)})
+    return JS_text.safe_substitute({'divnum': divnum, 'main_text': main_text})
